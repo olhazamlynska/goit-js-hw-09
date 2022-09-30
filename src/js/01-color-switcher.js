d54afbd8d163b72a2b8ceb1470e9a9a0202eaf54
olhazamlynska <olya.sonechko@gmail.com>
2022-09-30 13:00:10 +0300

    Update task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStartRef = document.querySelector('[data-start]');
+const btnStopRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
 
+let intervalId = null;
+
+function onBtnStartClick(event) {
+  intervalId = setInterval(() => {
+    const currentColor = getRandomHexColor();
+    bodyRef.style.backgroundColor = currentColor;
+    btnStartRef.disabled = true;
+    btnStopRef.disabled = false;
+  }, 1000);
+}
+
+function onBtnStopClick(event) {
+  clearInterval(intervalId);
+  btnStartRef.disabled = false;
+  btnStopRef.disabled = true;
+}
+btnStartRef.addEventListener('click', onBtnStartClick);
+btnStopRef.addEventListener('click', onBtnStopClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
